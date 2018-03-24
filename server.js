e8d81f692221af397b45885363cbb3937d5f7c88
Hans-Christiaan Braun <hc.braun.93@gmail.com>
2018-03-24 13:56:11 +0100

    Scraped results can now be written to csv file.

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const request = require('request-promise-native');
 const NOS_API_URL = 'https://lfverkiezingen2018rr.appspot.com/data/';
 const BUILD_ID = '3i4bsa';
 
+const SAVE_PATH = './raw_data/referendum_results.csv';
+
 const MAX_NR = 100;
 
 // let numbers = new Array(MAX_NR).fill(1).map((v, i) => i);
@@ -26,24 +28,24 @@ getReferendumResults = municipalityID => {
 getLineFromResult = result => {
   let id = result.cbs;
   let name = result.title;
-  let provence = result.provence;
+  let province = result.provence;
   let updated = result.updated;
 
-  let voters = result.totals.attendance.now.count;
+  let turnout = result.totals.attendance.now.count;
   let eligible = result.totals.voters.now.count;
   let percVoted = result.totals.attendance.now.perc;
 
   let votesFor = result.parties[0].now.count;
   let votesAgainst = result.parties[1].now.count;
-  let votesBlanco = voters - votesFor - votesAgainst;
+  let votesBlanco = turnout - votesFor - votesAgainst;
 
   let percAgainst = result.parties[0].now.perc;
   let percFor = result.parties[1].now.perc;
   let percBlanco = (100.0 - percAgainst - percFor).toFixed(1);
 
-  return [id, name, provence, updated, eligible, voters, percVoted, 
+  return [id, name, province, updated, eligible, turnout, percVoted, 
     votesFor, votesAgainst, votesBlanco, 
-    percAgainst, percFor, percBlanco].join(',');
+    percFor, percAgainst, percBlanco].join(';') + '\n';
 }
 
 scrapeResults = async () => {
@@ -58,26 +60,31 @@ scrapeResults = async () => {
     console.log(error);
   }); 
 
-  // Gather the list of referendum results for each municipality.
-  let promises = Promise.all(municipalityIDs.map(id => getReferendumResults(id)));
+  // Make a new file stream to write to.
+  let outputStream = fs.createWriteStream(SAVE_PATH);
 
-  // Parse each result to a line of comma-separated values.
-  return promises.then(results => {
-    return results.map(result => {
-      return getLineFromResult(result);
-    });
-  }).catch(error => console.error(error));
-  
-  // return await municipalityIDs.map(async id => {
-  //   let result = await getReferendumResults(id);
-  //   return getLineFromResult(result);  
-  // });
+  // Write CSV header.
+  outputStream.write(['id','name','province','updated','eligible','turnout','turnout percentage',
+    'votes for', 'votes against', 'none of the above', 
+    'percentage for', 'percentage against', 'percentage NOTA'].join(';') + '\n');
+
+  // Retrieve the results for each municipality and write
+  // them to the stream.
+  municipalityIDs.forEach(
+    id => {
+      getReferendumResults(id).then(
+        result => {
+          console.log(`- Retrieved results from ${result.title} (${result.cbs})`);
+          let line = getLineFromResult(result);
+          outputStream.write(line);
+        }
+      ).catch(
+        error => console.log(error)
+      );
+    }
+  )
 }
 
-scrapeResults().then(
-  lines => {
-    lines.forEach(line => console.log(line));
-  }
-);
+scrapeResults();
 
 
